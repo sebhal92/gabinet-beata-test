9551ef6e4cdc773816166b4a833e0f976820b6d4
sebhal92 <58229006+sebhal92@users.noreply.github.com>
2026-09-13 22:02:24 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,22 @@
-dupa
+module.exports = {
+  content: [
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./*.html",
+    "./assets/js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          hand: "#225abc",
+          spine: "#36b2b2"
+        }
+      }
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/container-queries")
+  ],
+};
